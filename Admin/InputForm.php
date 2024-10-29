5e6c34f64c460175e74b9446f749d74164ed7e8c
kenzogha <kenzoghaisanimudz@gmail.com>
2024-10-30 06:41:53 +0700

    admin

---
+++ Admin/InputForm.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Script/CSS/InputForm.css">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Questrial&display=swap"
+        rel="stylesheet" />
+    <link rel="Icon" type="image/x-icon" href="../Logo/KABAT.jpg">
+    <title>ProductInput</title>
+</head>
+
+<body>
+    <h1>Kabat<span>Coffee</span></h1>
+    <h2>InputProduct</h2>
+    <form action="ProductOutput.php" method="POST" autocomplete="off" enctype="multipart/form-data">
+        <label for="">Product Name</label>
+        <input type="text" name="name">
+        <label for="">Description</label>
+        <textarea name="desc" id=""></textarea>
+        <label for="">Image</label>
+        <input type="file" class="img" name="image">
+        <label for="">Price</label>
+        <input type="number" name="price">
+        <label for="">ID of the Category</label>
+        <input type="number" name="category">
+        <button>Submit</button>
+    </form>
+</body>
+
+</html>
---
+++ Admin/ProductOutput.php
@@ -0,0 +1,14 @@
+<?php
+    $name = $_POST['name'];
+    $desc = $_POST['desc'];
+    $image = $_POST['image'];
+    $price = $_POST['price'];
+    $category = $_POST['category'];
+
+    echo "<h1>$name</h1>";
+    echo "<h1>$desc</h1>";
+    echo "<h1>$image</h1>";
+    echo "<h1>$price</h1>";
+    echo "<h1>$category</h1>";
+
+?>
